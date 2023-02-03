f6e02e23e53c9fc8b0b15989a5d6266e5461c2e2
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-02-02 22:40:40 -0300

    script sortear numero

--- assets/js/sortear-numero.js
+++ assets/js/sortear-numero.js
@@ -1,6 +1,14 @@
-const numeroSecreto = gerarNumeroAleatorio()
+const menorValor = 250
+const maiorValor = 725
 
-function gerarNumeroAleatorio() {
+const numeroSecreto = gerarNumeroAleatorio(menorValor, maiorValor)
 
+function gerarNumeroAleatorio(min, max) {
+    
+    return parseInt(Math.random() * ((max + 1) - min) + min)
 }
 
+document.querySelector('[data-menor-valor]').innerHTML = menorValor
+document.querySelector('[data-maior-valor]').innerHTML = maiorValor
+
+console.log(numeroSecreto)
