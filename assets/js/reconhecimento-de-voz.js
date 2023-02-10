f9f0849a0ba93ed167b762a35af9b0cb68a79295
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-02-10 13:39:12 -0300

    conclusao

--- assets/js/reconhecimento-de-voz.js
+++ assets/js/reconhecimento-de-voz.js
@@ -1,22 +1,16 @@
 const chuteElemento = document.querySelector('[data-id="chute"]')
 const principalElemento = document.querySelector('[data-id="principal"]')
 
-// O Chrome atualmente oferece suporte ao reconhecimento de fala com propriedades prefixadas, incluímos estas linhas para alimentar os objetos corretos no Chrome e quaisquer implementações futuras que possam oferecer suporte aos recursos sem um prefixo
 const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
-// Define uma para controlar o reconhecimento de fala do nosso aplicativo
 const recognition = new SpeechRecognition()
-// Define o idioma do reconhecimento. Definir isso é uma boa prática e, portanto, recomendado
 recognition.lang = 'pt-Br'
 
-// Inicia o serviço de reconhecimento de fala ouvindo o áudio de entrada
 recognition.start()
-
-// Disparado quando o reconhecimento de fala retorna um resultado, chama a função onSpeek 
+ 
 recognition.addEventListener('result', onSpeak)
 
 function onSpeak(e) {
-    // Obtem uma string contendo a transcricao da palavra reconhecida
     const chute = e.results[0][0].transcript
 
     exibirChuteNaTela(chute)
